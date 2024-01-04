6d7a7240784b7b9b63638755864e6d2f4cbed6f5
Anas Pikri <116987481+MuhdAnass@users.noreply.github.com>
2024-01-04 21:44:01 +0800

    Add files via upload

---
+++ mvcprojectnew/app/views/activities/layout.php
@@ -0,0 +1,65 @@
+<!--begin::Content-->
+<div id="kt_app_content" class="app-content flex-column-fluid">
+    <!--begin::Content container-->
+    <div id="kt_app_content_container" class="app-container container-fluid">
+        <!--begin::Row-->
+
+
+        <!--Content area here-->
+
+        <?php
+                    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+                        $url = "https://";
+                    else
+                        $url = "http://";
+                    // Append the host(domain name, ip) to the URL.   
+                    $url .= $_SERVER['HTTP_HOST'];
+
+                    // Append the requested resource location to the URL   
+                    $url .= $_SERVER['REQUEST_URI'];
+                  
+                    ?>
+
+        <?php
+                 
+                    $c_url = URLROOT . "/activities"; 
+                    $t_url = URLROOT . "/activities/create"; 
+
+                    if (isset($data['activity']) && is_object($data['activity'])) {
+
+                    $u_url = URLROOT . "/activities/update/".$data['activity']->act_id; 
+
+                    }
+
+
+
+                    //error_reporting(0);
+                    if ($url == $c_url) {
+        
+                        require 'manage.php';
+                 
+                    }elseif($url == $t_url){
+
+                        require 'create.php';
+
+                    }elseif($url == $u_url){
+
+                        require 'update.php';
+
+                    }else{
+
+                    }
+
+                    ?>
+
+                    
+        <!--End of Content area here-->
+        
+
+        <!--end::Row-->
+    </div>
+    <!--end::Content container-->
+</div>
+<!--end::Content-->
+
+
---
+++ mvcprojectnew/app/views/activities/manage.php
@@ -0,0 +1,95 @@
+<div class="card shadow-sm">
+    <div class="card-header">
+        <h3 class="card-title">Manage Activities</h3>
+        <div class="card-toolbar">
+            <?php if(isLoggedIn()): ?>
+            <a href="<?php echo URLROOT;?>/activities/create" class="btn btn-light-primary">Create</a>
+            <?php endif; ?>
+        </div>
+    </div>
+    <div class="card-body">
+  
+    <div class="table-responsive">
+            <table id="kt_datatable_posts" class="table table-row-bordered gy-5">
+                <thead>
+                    <tr class="fw-semibold fs-6 text-muted">
+                        <th>Title</th>
+                        <th>Content</th>
+                        <th>Date</th>
+                        <th>Action</th>
+                     
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($data['activities'] as $activity): ?>
+                    <tr>
+                        <td><?php echo $activity->act_title; ?></td>
+                        <td><?php echo $activity->act_desc; ?></td>
+                        <td><?php echo date('F j h:m', strtotime($activity->act_datetime)); ?></td>
+                        <td> <a href="<?php echo URLROOT . "/activities/update/" . $activity->act_id ?>"
+                                class="btn btn-light-warning">Update</a> 
+                            
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                data-bs-target="#kt<?php echo $activity->act_id?>">
+                                Delete
+                            </button>
+
+                            <div class="modal fade" tabindex="-1" id="kt<?php echo $activity->act_id?>">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h3 class="modal-title">Delete Activity</h3>
+
+                                            <!--begin::Close-->
+                                            <div class="btn btn-icon btn-sm btn-active-light-primary ms-2"
+                                                data-bs-dismiss="modal" aria-label="Close">
+                                                <i class="ki-duotone ki-cross fs-1"><span class="path1"></span><span
+                                                        class="path2"></span></i>
+                                            </div>
+                                            <!--end::Close-->
+                                        </div>
+
+                                        <div class="modal-body">
+
+                                            Are you sure want to delete this activity?
+
+                                        </div>
+
+                                        <div class="modal-footer">
+                                            <form action="<?php echo URLROOT . "/activities/delete/" . $activity->act_id; ?>" method="POST">
+                                                <!-- <input type="hidden" id="expenses" name="expenses" value="expenses"> -->
+                                                <button type="button" class="btn btn-light-primary font-weight-bold"
+                                                    data-dismiss="modal">Close</button>
+                                                <button type="submit"
+                                                    class="btn btn-primary font-weight-bold">Delete</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </td>
+                        
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
+
+        <script>
+        $(document).ready(function() {
+            var table = $('#kt_datatable_posts').DataTable({
+
+            });
+        });
+        </script>
+
+
+    </div>
+    <div class="card-footer">
+        Footer
+    </div>
+</div>
